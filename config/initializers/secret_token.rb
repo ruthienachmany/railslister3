ac39a6dacd690e06c69a839b79c92247019fbd71
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-07-08 11:46:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railslister3::Application.config.secret_key_base = 'b4bb2915f03ee9af740ba82c3bdfb77a60f46c0dc3a6c7558baf57f56d1bae09d0ce2ff676ca6da8de0bcef352861dc6153140dc323517a14a0f3456fb9f89a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railslister3::Application.config.session_store :cookie_store, key: '_railslister3_session'
